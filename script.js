ae3914d67e875473ef767ec4c65920d1ae2d7d6f
Elayt <84461884+bd-ans@users.noreply.github.com>
2022-08-11 23:21:55 +0500

    .

--- script.js
+++ script.js
@@ -34,8 +34,70 @@ let showHide = function() {
 }
 // elFormBtn && elFormClearBtn show or hide function
 elBtn.addEventListener('click', function () {
-  let inputValue = elInput.value.trim();
-
+  if (elFormCheckInput.checked) {
+    let inputValue = elInput.value.trim();
+    if (inputValue === '' || inputValue === null || inputValue === undefined || Number(inputValue) || inputValue.length < 2) {
+      elInput.setAttribute('placeholder', 'Iltimos mahsulot nomini kiriting');
+      elInput.classList.add('is-invalid');
+      elInput.value = null;
+      elInput.focus();
+    } else {
+      if (list.includes(inputValue)) { // list includes check function
+        elInput.setAttribute('placeholder', 'Bu mahsulot oldin qo`shilgan');
+        elInput.classList.add('is-invalid');
+        elInput.value = null;
+        elInput.focus();
+      } else {
+        elInput.setAttribute('placeholder', 'davom eting');
+        list.unshift(inputValue);
+        let elItem = document.createElement('li');
+        elInput.classList.remove('is-invalid');
+        showHide();
+  
+        for (mahsulotNomi of list) { // main for function
+          elItem.setAttribute('class', 'text-light d-flex align-items-center border-bottom py-1 shadow-sm');
+          mahsulotNomi = inputValue;
+          elItem.textContent = mahsulotNomi;
+          ellist.insertBefore(elItem, ellist.firstChild);
+          
+          let deleteBtn = document.createElement('button'); // delete list items btn
+          deleteBtn.textContent = 'O\'chirish';
+          deleteBtn.setAttribute('class', 'btn btn-info shadow-lg rounded-3 border-light text-light ms-auto me-1 btn-sm');
+          elItem.appendChild(deleteBtn);
+  
+          deleteBtn.addEventListener('click', function () {
+            let elItemTextCont = elItem.firstChild.textContent;
+            ellist.removeChild(elItem);
+            list.splice(list.indexOf(elItemTextCont), 1);
+            showHide();
+          }); // delete list items btn
+          
+          elFormClearBtn.addEventListener('click', function() { // clear btn function
+            list.length = 0;
+            ellist.innerHTML = '';
+            elFormInput.value = list;
+            showHide();
+          }); // clear btn function end
+          
+          elFormBtn.addEventListener('click', function() { // send form values btn function
+            elFormInput.value = list;
+          }); // send form values btn function end
+  
+        } // main for function end
+        
+        elInput.value = null;
+        elInput.focus();
+        
+        function updateScroll(){ // update scroll function
+          var element = document.getElementById("mainList");
+          element.scrollTop = element.scrollHeight;
+        }
+        updateScroll(); // update scroll function end
+        
+        } // list includes check end
+      }
+  } else {                    //fcgvhj tyhij ty ujiuyu vtubh jugvy gbhnjhu gyvgbh jugyv gbhnj gvbh
+    let inputValue = elInput.value.trim();
   if (inputValue === '' || inputValue === null || inputValue === undefined || Number(inputValue) || inputValue.length < 2) {
     elInput.setAttribute('placeholder', 'Iltimos mahsulot nomini kiriting');
     elInput.classList.add('is-invalid');
@@ -56,6 +118,7 @@ elBtn.addEventListener('click', function () {
 
       for (mahsulotNomi of list) { // main for function
         elItem.setAttribute('class', 'text-light d-flex align-items-center border-bottom py-1 shadow-sm');
+        mahsulotNomi = inputValue;
         elItem.textContent = mahsulotNomi;
         ellist.appendChild(elItem);
         
@@ -95,4 +158,9 @@ elBtn.addEventListener('click', function () {
       
       } // list includes check end
     }
+  }
 });
+
+setInterval(function() {
+  console.log(list);
+} , 1000);
