02377b4774ad85410acc679617e9341c24d7fb96
Elayt <84461884+bd-ans@users.noreply.github.com>
2022-09-01 16:27:31 +0500

    .

--- js/script.js
+++ js/script.js
@@ -2,24 +2,40 @@ let elInput = $('.input');
 let elFormCheckInput = $('.form-check-input');
 let elFormInput = $('.form-input');
 let elBtn = $('.js-add-to-list-btn');
-let elFormBtn = $('.form-btn');
-let elFormClearBtn = $('.form-clear-btn');
+let elFormBuyBtn = $('.js-but-btn');
+let elFormClearBtn = $('.js-form-clear-btn');
 let ellist = $('.list');
 
+// input keyup event
 elInput.addEventListener('keyup', function(e) {
   if (e.keyCode === 13) {
     elBtn.click();
   }
 } );
+// list array
+let list = [];
 
-let list = [
-  'olma',
-  'nok'
-];
-
+// localstroage set function
 list = JSON.parse(localStorage.getItem('list')) || list;
 
+// forEach function
 list.forEach(function(item) {
+  // clear && but
+  if (list.length <= 0) {
+    elFormBuyBtn.classList.add('opacity-50');
+    elFormBuyBtn.classList.add('disabled');
+    elFormBuyBtn.classList.remove('opacity-100');
+    elFormClearBtn.classList.add('opacity-50');
+    elFormClearBtn.classList.add('disabled');
+    elFormClearBtn.classList.remove('opacity-100');
+  } else {
+    elFormBuyBtn.classList.remove('opacity-50');
+    elFormBuyBtn.classList.remove('disabled');
+    elFormBuyBtn.classList.add('opacity-100');
+    elFormClearBtn.classList.remove('opacity-50');
+    elFormClearBtn.classList.remove('disabled');
+    elFormClearBtn.classList.add('opacity-100');
+  }
   let elItem = document.createElement('li');
   elItem.setAttribute('class', 'text-light d-flex align-items-center border-bottom py-1 shadow-sm');
   elItem.classList.add('list-item');
@@ -28,45 +44,54 @@ list.forEach(function(item) {
   // delete btn
   let deleteBtn = document.createElement('button'); // delete list items btn
   deleteBtn.textContent = 'O\'chirish';
-  if (elFormCheckInput.checked) {
-    deleteBtn.setAttribute('class', 'btn btn-warning shadow-lg rounded-3 border-light text-black ms-auto me-1 btn-sm');
-  } else {
   deleteBtn.setAttribute('class', 'btn btn-info shadow-lg rounded-3 border-light text-light ms-auto me-1 btn-sm');
-  }
   elItem.appendChild(deleteBtn);
   // delete btn function
   deleteBtn.addEventListener('click', function () {
     let elItemTextCont = elItem.firstChild.textContent;
-    ellist.removeChild(elItem);
-    list.splice(list.indexOf(elItemTextCont), 1);
+    const indexm = list.indexOf(elItemTextCont);
+    list.splice(indexm, 1);
+    elItem.remove();
+    localStorage.setItem('list', JSON.stringify(list));
     showHide();
-    localStorage.removeItem('list');
   }); // delete list items btn
 } );
+// forEach function end
+
+// input chenge check event
+if (localStorage.getItem('formCheckInput') === 'true') {
+  elFormCheckInput.checked = true;
+} else {
+  elFormCheckInput.checked = false;
+}
+// on input change event
+elFormCheckInput.addEventListener('change', function() {
+  localStorage.setItem('formCheckInput', elFormCheckInput.checked);
+});
 
 elInput.setAttribute('maxlength', '25');
-// elFormBtn && elFormClearBtn show or hide function
+// elFormBuyBtn && elFormClearBtn show or hide function
 let showHide = function() {
   if (list.length <= 0) {
-    elFormBtn.classList.add('opacity-50');
-    elFormBtn.classList.add('disabled');
-    elFormBtn.classList.remove('opacity-100');
+    elFormBuyBtn.classList.add('opacity-50');
+    elFormBuyBtn.classList.add('disabled');
+    elFormBuyBtn.classList.remove('opacity-100');
     elFormClearBtn.classList.add('opacity-50');
     elFormClearBtn.classList.add('disabled');
     elFormClearBtn.classList.remove('opacity-100');
   } else {
-    elFormBtn.classList.remove('opacity-50');
-    elFormBtn.classList.remove('disabled');
-    elFormBtn.classList.add('opacity-100');
+    elFormBuyBtn.classList.remove('opacity-50');
+    elFormBuyBtn.classList.remove('disabled');
+    elFormBuyBtn.classList.add('opacity-100');
     elFormClearBtn.classList.remove('opacity-50');
     elFormClearBtn.classList.remove('disabled');
     elFormClearBtn.classList.add('opacity-100');
   }
 }
-// elFormBtn && elFormClearBtn show or hide function
 // main function
 elBtn.addEventListener('click', function () {
   let inputValue = elInput.value.trim();
+  // input value check
   if (inputValue === '' || inputValue === null || inputValue === undefined || Number(inputValue) || inputValue.length < 2) {
     elInput.setAttribute('placeholder', 'Iltimos mahsulot nomini kiriting');
     elInput.classList.add('is-invalid');
@@ -88,8 +113,8 @@ elBtn.addEventListener('click', function () {
       let elItem = document.createElement('li');
       elInput.classList.remove('is-invalid');
       showHide();
-
-      for (mahsulotNomi of list) { // main for function
+      // main for function
+      for (mahsulotNomi of list) {
         if (elFormCheckInput.checked) {
           elItem.setAttribute('class', 'text-warning d-flex align-items-center border-bottom py-1 shadow-lg');
         } else {
@@ -114,39 +139,35 @@ elBtn.addEventListener('click', function () {
         // delete btn function
         deleteBtn.addEventListener('click', function () {
           let elItemTextCont = elItem.firstChild.textContent;
-          ellist.removeChild(elItem);
-          list.splice(list.indexOf(elItemTextCont), 1);
+          const indexm = list.indexOf(elItemTextCont);
+          list.splice(indexm, 1);
+          elItem.remove();
+          localStorage.setItem('list', JSON.stringify(list));
           showHide();
-          localStorage.removeItem('list');
-        }); // delete list items btn
-        
+        });
+        // delete list items btn
         elFormClearBtn.addEventListener('click', function() { // clear btn function
           list.length = 0;
           ellist.innerHTML = null;
           elFormInput.value = list;
           showHide();
-        }); // clear btn function end
-        
-        elFormBtn.addEventListener('click', function() { // send form values btn function
+        });
+        // clear btn function end
+        elFormBuyBtn.addEventListener('click', function() { // send form values btn function
           elFormInput.value = list;
-        }); // send form values btn function end
-
-      } // main for function end
+        });
+        // send form values btn function end
+      }
       //local storage function
       localStorage.setItem('list', JSON.stringify(list));
       elInput.value = null;
       elInput.focus();
-      
-      function updateScroll(){ // update scroll function
+      // update scroll function
+      function updateScroll(){
         var element = document.getElementById("mainList");
         element.scrollTop = element.scrollHeight;
       }
-      updateScroll(); // update scroll function end
-      } // list includes check end
+      updateScroll();
+      }
     }
-
-}); // main function end
-setInterval(function() {
-  console.log(list);
-} , 1500);
-// localStorage.removeItem('mahsultolar');
+});
