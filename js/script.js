673bcc729269649b3cf8f766d2c922f78362dd66
Elayt <84461884+bd-ans@users.noreply.github.com>
2022-09-01 16:36:00 +0500

    .

--- js/script.js
+++ js/script.js
@@ -20,22 +20,6 @@ list = JSON.parse(localStorage.getItem('list')) || list;
 
 // forEach function
 list.forEach(function(item) {
-  // clear && but
-  if (list.length <= 0) {
-    elFormBuyBtn.classList.add('opacity-50');
-    elFormBuyBtn.classList.add('disabled');
-    elFormBuyBtn.classList.remove('opacity-100');
-    elFormClearBtn.classList.add('opacity-50');
-    elFormClearBtn.classList.add('disabled');
-    elFormClearBtn.classList.remove('opacity-100');
-  } else {
-    elFormBuyBtn.classList.remove('opacity-50');
-    elFormBuyBtn.classList.remove('disabled');
-    elFormBuyBtn.classList.add('opacity-100');
-    elFormClearBtn.classList.remove('opacity-50');
-    elFormClearBtn.classList.remove('disabled');
-    elFormClearBtn.classList.add('opacity-100');
-  }
   let elItem = document.createElement('li');
   elItem.setAttribute('class', 'text-light d-flex align-items-center border-bottom py-1 shadow-sm');
   elItem.classList.add('list-item');
@@ -54,9 +38,40 @@ list.forEach(function(item) {
     elItem.remove();
     localStorage.setItem('list', JSON.stringify(list));
     showHide();
-  }); // delete list items btn
+  });
 } );
 // forEach function end
+
+// clear && buy btn show or hide function
+if (list.length <= 0) {
+  elFormBuyBtn.classList.add('opacity-50');
+  elFormBuyBtn.classList.add('disabled');
+  elFormBuyBtn.classList.remove('opacity-100');
+  elFormClearBtn.classList.add('opacity-50');
+  elFormClearBtn.classList.add('disabled');
+  elFormClearBtn.classList.remove('opacity-100');
+} else {
+  elFormBuyBtn.classList.remove('opacity-50');
+  elFormBuyBtn.classList.remove('disabled');
+  elFormBuyBtn.classList.add('opacity-100');
+  elFormClearBtn.classList.remove('opacity-50');
+  elFormClearBtn.classList.remove('disabled');
+  elFormClearBtn.classList.add('opacity-100');
+}
+
+// cler btn function
+elFormClearBtn.addEventListener('click', function() { // clear btn function
+  list.length = 0;
+  ellist.innerHTML = null;
+  elFormInput.value = list;
+  showHide();
+  localStorage.setItem('list', JSON.stringify(list));
+});
+
+// buy btn function
+elFormBuyBtn.addEventListener('click', function() { // send form values btn function
+  elFormInput.value = list;
+});
 
 // input chenge check event
 if (localStorage.getItem('formCheckInput') === 'true') {
@@ -64,6 +79,7 @@ if (localStorage.getItem('formCheckInput') === 'true') {
 } else {
   elFormCheckInput.checked = false;
 }
+
 // on input change event
 elFormCheckInput.addEventListener('change', function() {
   localStorage.setItem('formCheckInput', elFormCheckInput.checked);
@@ -71,6 +87,7 @@ elFormCheckInput.addEventListener('change', function() {
 
 elInput.setAttribute('maxlength', '25');
 // elFormBuyBtn && elFormClearBtn show or hide function
+
 let showHide = function() {
   if (list.length <= 0) {
     elFormBuyBtn.classList.add('opacity-50');
@@ -88,9 +105,11 @@ let showHide = function() {
     elFormClearBtn.classList.add('opacity-100');
   }
 }
+
 // main function
 elBtn.addEventListener('click', function () {
   let inputValue = elInput.value.trim();
+
   // input value check
   if (inputValue === '' || inputValue === null || inputValue === undefined || Number(inputValue) || inputValue.length < 2) {
     elInput.setAttribute('placeholder', 'Iltimos mahsulot nomini kiriting');
@@ -113,6 +132,7 @@ elBtn.addEventListener('click', function () {
       let elItem = document.createElement('li');
       elInput.classList.remove('is-invalid');
       showHide();
+
       // main for function
       for (mahsulotNomi of list) {
         if (elFormCheckInput.checked) {
@@ -127,6 +147,7 @@ elBtn.addEventListener('click', function () {
         } else {
         ellist.appendChild(elItem);
         }
+
         // delete btn
         let deleteBtn = document.createElement('button'); // delete list items btn
         deleteBtn.textContent = 'O\'chirish';
@@ -136,6 +157,7 @@ elBtn.addEventListener('click', function () {
         deleteBtn.setAttribute('class', 'btn btn-info shadow-lg rounded-3 border-light text-light ms-auto me-1 btn-sm');
         }
         elItem.appendChild(deleteBtn);
+
         // delete btn function
         deleteBtn.addEventListener('click', function () {
           let elItemTextCont = elItem.firstChild.textContent;
@@ -145,23 +167,27 @@ elBtn.addEventListener('click', function () {
           localStorage.setItem('list', JSON.stringify(list));
           showHide();
         });
-        // delete list items btn
+
+        // cler btn function
         elFormClearBtn.addEventListener('click', function() { // clear btn function
           list.length = 0;
           ellist.innerHTML = null;
           elFormInput.value = list;
           showHide();
+          localStorage.setItem('list', JSON.stringify(list));
         });
-        // clear btn function end
+
+        // buy btn function
         elFormBuyBtn.addEventListener('click', function() { // send form values btn function
-          elFormInput.value = list;
+          elFormInput.value = list.join(', ');
         });
-        // send form values btn function end
       }
+      
       //local storage function
       localStorage.setItem('list', JSON.stringify(list));
       elInput.value = null;
       elInput.focus();
+
       // update scroll function
       function updateScroll(){
         var element = document.getElementById("mainList");
